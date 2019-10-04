08734fd409f7acb64e5618950ad54d6d19a463ef
Leonel Vergara <leovergarah@gmail.com>
2019-10-03 22:50:02 -0400

    (Added) fh -> kel & kel -> fh tests

--- tests.py
+++ tests.py
@@ -9,6 +9,18 @@ class TestConversions(unittest.TestCase):
     def test_convertCelsiusToFahrenheit(self, celsius):
         return 0.0
 
+    def convertFahrenheitToCelsius(self, fahrenheit):
+        return 0.0
+
+    def convertFahrenheitToKelvin(self, fahrenheit):
+        return 0.0
+
+    def convertKelvinToFahrenheit(self, kelvin):
+        return 0.0
+
+    def convertKelvinToCelsius(self, kelvin):
+        return 0.0 
+
 
 if __name__ == '__main__':
     unittest.main()
