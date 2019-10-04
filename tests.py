cb4e85175e53a1b5ce19202c17c723495f5913fa
Leonel Vergara <leovergarah@gmail.com>
2019-10-03 21:56:26 -0400

    (Added) basic scaffolding for test

---
+++ tests.py
@@ -0,0 +1,14 @@
+import unittest
+
+class TestConversions(unittest.TestCase):
+    """Test Conversion tests"""
+
+    def test_convertCelsiusToKelvin(self, celsius):
+        return 0.0 
+    
+    def test_convertCelsiusToFahrenheit(self, celsius):
+        return 0.0
+
+
+if __name__ == '__main__':
+    unittest.main()
