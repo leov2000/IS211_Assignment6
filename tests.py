bf8258022c7ba1a692244846aa5254f21bb0a69c
Leonel Vergara <leovergarah@gmail.com>
2019-10-05 00:32:25 -0400

    (Refactored) tests.py spec runner

--- tests.py
+++ tests.py
@@ -2,4 +2,4 @@ import unittest
 
 if __name__ == '__main__':
     suite = unittest.TestLoader().discover('tests', pattern='*test*.py')
-    result = unittest.TextTestRunner(verbosity=2).run(suite)
+    runner = unittest.TextTestRunner(verbosity=2).run(suite)
