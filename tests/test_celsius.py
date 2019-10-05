4b528cc600a28c1e8f257afcf21aa5cb93c1a90e
Leonel Vergara <leovergarah@gmail.com>
2019-10-04 23:42:47 -0400

    (Refactored) docstring

--- tests/test_celsius.py
+++ tests/test_celsius.py
@@ -8,7 +8,7 @@ sys.path.append(os.path.join(module_dir, '../'))
 from conversions import convertCelsiusToFahrenheit, convertCelsiustoKelvin
 
 class TestCelsius(unittest.TestCase):
-    """Test Conversion tests"""
+    """Celsius Conversion Tests"""
     def test_celsius_to_kelvin_with_300_should_be_573_15(self):
         self.assertEqual(convertCelsiustoKelvin(300.00), 573.15)
         
