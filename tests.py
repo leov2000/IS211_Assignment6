3eb3115beaf2627db73f939ce1204b0ee77e8017
Leonel Vergara <leovergarah@gmail.com>
2019-10-04 23:41:11 -0400

    (Added) test suite runner

--- tests.py
+++ tests.py
@@ -1,26 +1,5 @@
 import unittest
 
-class TestConversions(unittest.TestCase):
-    """Test Conversion tests"""
-
-    def test_convertCelsiusToKelvin(self, celsius):
-        return 0.0 
-    
-    def test_convertCelsiusToFahrenheit(self, celsius):
-        return 0.0
-
-    def convertFahrenheitToCelsius(self, fahrenheit):
-        return 0.0
-
-    def convertFahrenheitToKelvin(self, fahrenheit):
-        return 0.0
-
-    def convertKelvinToFahrenheit(self, kelvin):
-        return 0.0
-
-    def convertKelvinToCelsius(self, kelvin):
-        return 0.0 
-
-
 if __name__ == '__main__':
-    unittest.main()
+    suite = unittest.TestLoader().discover('tests', pattern='*test*.py')
+    result = unittest.TextTestRunner(verbosity=2).run(suite)
