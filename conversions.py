368df25ec5597d829e7bb11639ed5374b6733395
Leonel Vergara <leovergarah@gmail.com>
2019-10-06 15:39:39 -0400

    (Added) doc-string to conversions

--- conversions.py
+++ conversions.py
@@ -1,36 +1,78 @@
 def convertCelsiustoKelvin(celsius):
     """
-    °C + 273.15 = K
+    A conversion function used to convert celsius to kelvin
+    using this formula:  °C + 273.15 = K
+
+    Args:
+        celsius(float)
+    
+    Returns:
+        A converted kelvin value.
     """
     return celsius + 273.15
 
 def convertCelsiusToFahrenheit(celsius):
     """
-    (°C × 9/5) + 32 = °F
+    A conversion function used to convert celsius to fahrenheit
+    using this formula: (°C × 9/5) + 32 = °F
+
+    Args:
+        celsius(float)
+    
+    Returns:
+        A converted fahrenheit value.
     """
     return (celsius * 9 / 5) + 32
 
 
 def convertFahrenheitToCelsius(fahrenheit):
     """
-    (°F − 32) × 5/9 = °C
+    A conversion function used to convert fahrenheit to celsius
+    using this formula: (°F − 32) × 5/9 = °C
+
+    Args:
+        fahrenheit(float)
+    
+    Returns:
+        A converted celsius value.
     """
     return (fahrenheit - 32) * (5 / 9)
 
 def convertFahrenheitToKelvin(fahrenheit):
     """
-    (°F − 32) × 5/9 + 273.15 = K
+    A conversion function that converts fahrenheit to kelvin
+    using this formula: (°F − 32) × 5/9 + 273.15 = K
+
+    Args:
+        fahrenheit(float)
+    
+    Returns:
+        A converted kelvin value.
     """
     return round((fahrenheit - 32) * (5 / 9) + 273.15, 3)
 
 def convertKelvinToFahrenheit(kelvin):
     """
-    (K − 273.15) × 9/5 + 32 = °F
+    A conversion function that converts kelvin to fahrenheit
+    using this formula: (K − 273.15) × 9/5 + 32 = °F
+
+    Args:
+        kelvin(float)
+    
+    Returns:
+        A converted fahrenheit value.
     """
     return round((kelvin - 273.15) * (9 / 5) + 32, 2) 
 
 def convertKelvinToCelsius(kelvin):
     """
-    K − 273.15 = °C
+    A conversion function that converts kelvin to celsius
+    using this formula: K − 273.15 = °C
+
+    Args:
+        kelvin(float)
+
+    Returns:
+        A converted celsius value. 
     """
     return round(kelvin - 273.15, 2)
